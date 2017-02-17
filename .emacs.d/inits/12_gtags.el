08c1723151aa03d49ceeb30d6dc7b3952f3d15b8
yaskoh <yasutake.kohei@gmail.com>
2017-02-18 01:16:22 +0900

    Add gtags settings

---
+++ .emacs.d/inits/12_gtags.el
@@ -0,0 +1,9 @@
+(require 'gtags)
+(setq gtags-path-style 'relative)
+(setq gtags-mode-hook
+      '(lambda ()
+         (local-set-key "\M-t" 'gtags-find-tag)
+         (local-set-key "\M-r" 'gtags-find-rtag)
+         (local-set-key "\M-s" 'gtags-find-symbol)
+         (local-set-key "\C-t" 'gtags-pop-stack)
+         ))
