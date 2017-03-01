100b5dfad5c06462ec9ab44f830e09aeadc92748
yaskoh <yasutake.kohei@gmail.com>
2017-03-02 00:57:17 +0900

    Modify magit path settings on windows

--- .emacs.d/inits/99_magit.el
+++ .emacs.d/inits/99_magit.el
@@ -5,7 +5,5 @@
 ;; ================================================
 (when (or (eq system-type 'darwin)
           (eq system-type 'gnu/linux))
-  ;(setq exec-path (append '("C:\\msys64\\usr\\bin") exec-path))
-  ;(setq magit-git-executable "C:\\msys64\\usr\\bin\\git.exe")
   (require 'magit))
 (bind-key "C-x g" 'magit-status)
--- .emacs.d/inits/windows-settings.el
+++ .emacs.d/inits/windows-settings.el
@@ -1,25 +1,33 @@
-;; ================================================
+;; ================================================================
 ;; Windowsでのフォント設定を変更
 ;; 2016/4/15
-;; ================================================
+;; ================================================================
 (when (eq system-type 'windows-nt)
   (custom-set-faces
    '(default ((t (:family "Ricty Diminished" :foundry "outline" :slant normal :weight normal :height 115 :width normal))))))
 
-;; ================================================
+;; ================================================================
 ;; Windowsでのgrep/find利用のためパスを変更
 ;; 2016/4/15
-;; ================================================
+;; ================================================================
 ;(when (eq system-type 'windows-nt)
 ;  (setenv "PATH" (concat "C:\\msys64\\usr\\bin;" (getenv "PATH")))
 ;  (setq find-program "C:\\msys64\\usr\\bin\\find.exe"
 ;        grep-program "C:\\msys64\\usr\\bin\\grep.exe"))
 
-;; ================================================
+
+;; ================================================================
+;; magit path設定
+;; ================================================================
+;(setq exec-path (append '("C:\\msys64\\usr\\bin") exec-path))
+;(setq magit-git-executable "C:\\msys64\\usr\\bin\\git.exe")
+
+
+;; ================================================================
 ;; Serverをスタートしておく
 ;; ※windowsではserverフォルダの所有者権限が必要な模様。
 ;; http://stackoverflow.com/questions/885793/emacs-error-when-calling-server-start
-;; ================================================
+;; ================================================================
 (when (eq system-type 'windows-nt)
   (require 'server)
   (unless (server-running-p)
