4ac333a2b58ff25ac87a24128552ce89d2d7a8a6
yaskoh <yasutake.kohei@gmail.com>
2017-04-22 11:02:38 +0900

    split "mode" file to "system" and "added", then rename files

---
+++ .emacs.d/inits/10_mode_system.el
@@ -0,0 +1,22 @@
+;; ================================================================
+;; org-mode
+;; 2014/4/6
+;; http://orgmode.org/ja/
+;; ================================================================
+;; default mode on new buffers
+(setq default-major-mode 'org-mode)
+
+(require 'org)
+;(require 'org-install)
+(bind-key "C-c l" 'org-store-link org-mode-map)
+(bind-key "C-c c" 'org-capture org-mode-map)
+(bind-key "C-c a" 'org-agenda org-mode-map)
+(bind-key "C-c b" 'org-iswitchb org-mode-map)
+
+;; ================================================================
+;; nxml-mode
+;; 2017/4/9
+;; ================================================================
+(require 'nxml-mode)
+(setq nxml-slash-auto-complete-flag t) ;completion by </
+(bind-key "C-<return>" 'nxml-complete nxml-mode-map)
